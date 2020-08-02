6e61547c7b7266e705649de733a753b65318d9af
vinibispo <vini.bispo015@gmail.com>
2020-08-02 19:33:23 -0300

    Adding if name main

--- getPlanetById.py
+++ getPlanetById.py
@@ -1,19 +1,25 @@
 def get_planet_name(id):
-        name: " "
-        if id == 1: 
-           name = "Mercury"
-        if id == 2:
-           name = "Venus"
-        if id == 3: 
-           name = "Earth"
-        if id ==  4: 
-           name = "Mars"
-        if id == 5: 
-           name = "Jupiter"
-        if id ==  6: 
-            name = "Saturn"
-        if id == 7: 
-           name = "Uranus"  
-        if id ==  8: 
-           name = "Neptune"
-        return name
+  name: " "
+  if id == 1: 
+     name = "Mercury"
+  if id == 2:
+     name = "Venus"
+  if id == 3: 
+     name = "Earth"
+  if id ==  4: 
+     name = "Mars"
+  if id == 5: 
+     name = "Jupiter"
+  if id ==  6: 
+      name = "Saturn"
+  if id == 7: 
+     name = "Uranus"  
+  if id ==  8: 
+     name = "Neptune"
+  return name
+
+
+if __name__ == "__main__":
+  id = int(input())
+  answer = get_planet_name(id)
+  print(answer)
--- sumOfPositive.py
+++ sumOfPositive.py
@@ -6,3 +6,12 @@ def positive_sum(arr):
         else: 
              u += 0  
     return u
+
+if __name__ == "__main__":
+  times = int(input("How much numbers has in this list? "))
+  arr = []
+  for i in range(0, times):
+    number = int(input("Number {}: ".format((i + 1))))
+    arr.append(number)
+  answer = positive_sum(arr)
+  print(answer)
